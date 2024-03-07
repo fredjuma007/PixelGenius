1dfbcca782ee810791f529427eb73b73180eebfa
fred <fredjuma8@gmail.com>
2024-03-07 20:49:41 +0300

    authentication(clerk)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,12 @@
-import { UserButton } from '@clerk/nextjs'
+import { UserButton } from "@clerk/nextjs";
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-    <div>Home</div>
+    <p>Home</p>
 
-    <UserButton afterSignOutUrl='/'/>
+    <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
